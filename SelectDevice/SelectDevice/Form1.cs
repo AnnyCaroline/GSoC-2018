6e77d9892c731a65b933a86c26bc5076ae3b90d0
Anny Caroline <annycarolinegnr@gmail.com>
2018-05-23 14:44:14 -0300

    fix bugs in SelectDevice app

--- SelectDevice/SelectDevice/Form1.cs
+++ SelectDevice/SelectDevice/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using System.Management;
+using System.Reflection;
 
 namespace SelectDevice
 {
@@ -46,35 +48,39 @@ namespace SelectDevice
             ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2",
                 "SELECT * FROM Win32_PnPEntity WHERE ClassGuid=\"{4d36e978-e325-11ce-bfc1-08002be10318}\"");
 
-            // Search all serial ports
-            foreach (ManagementObject queryObj in searcher.Get())
+            // Read file
+            try
             {
-                // Parse the data
-                if (null != queryObj["Name"])
-                {
-                    //https://stackoverflow.com/questions/24135006/regex-that-match-any-character-inside-a-parenthesis
-                    port = regex("(?<=" + Regex.Escape("(") + ")[^)]*(?=" + Regex.Escape(")") + ")", queryObj["Name"].ToString());
-                }
+                #if DEBUG
+                    string text = System.IO.File.ReadAllText(@"C:\Users\Anny\Desktop\ceu-maker\ceu-maker\arduino-1.8.3\hardware\arduino\avr\boards.txt");
+                #else
+                    // get path of the executing assembly
+                    string currentPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-                if (null != queryObj["PNPDeviceID"])
-                {
-                    //https://stackoverflow.com/questions/3926451/how-to-match-but-not-capture-part-of-a-regex?rq=1
-                    vid = regex("(?<=VID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
-                    pid = regex("(?<=PID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
-                    sn = regex("([0-9a-fA-F]{5,})", queryObj["PNPDeviceID"].ToString());
+                    string text = System.IO.File.ReadAllText(currentPath + @"\..\arduino-1.8.3\hardware\arduino\avr\boards.txt");
+                #endif
 
-                    if (!string.IsNullOrEmpty(port) && !string.IsNullOrEmpty(vid) && !string.IsNullOrEmpty(pid) && !string.IsNullOrEmpty(sn))
+                // Search all serial ports
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    // Parse the data
+                    if (null != queryObj["Name"])
                     {
-                        counter++;
+                        //https://stackoverflow.com/questions/24135006/regex-that-match-any-character-inside-a-parenthesis
+                        port = regex("(?<=" + Regex.Escape("(") + ")[^)]*(?=" + Regex.Escape(")") + ")", queryObj["Name"].ToString());
+                    }
 
-                        try
+                    if (null != queryObj["PNPDeviceID"])
+                    {
+                        //https://stackoverflow.com/questions/3926451/how-to-match-but-not-capture-part-of-a-regex?rq=1
+                        vid = regex("(?<=VID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                        pid = regex("(?<=PID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                        sn = regex("([0-9a-fA-F]{5,})", queryObj["PNPDeviceID"].ToString());
+
+                        if (!string.IsNullOrEmpty(port) && !string.IsNullOrEmpty(vid) && !string.IsNullOrEmpty(pid) && !string.IsNullOrEmpty(sn))
                         {
-                            #if DEBUG
-                                string text = System.IO.File.ReadAllText(@"C:\Users\AnnyC\Desktop\ceu-maker\ceu-maker\arduino-1.8.3\hardware\arduino\avr\boards.txt");
-                            #else
-                                string text = System.IO.File.ReadAllText(@"..\arduino-1.8.3\hardware\arduino\avr\boards.txt");
-                            #endif
-                        
+                            counter++;
+
                             //Pega as duas linhas correspondentes ao VID e PID
                             string a = "";
                             a = regex("[a-zA-Z]+" + Regex.Escape(".") + "vid" + Regex.Escape(".") + "[0-9]=0x" + vid + "(\r)(\n)[a-zA-Z]+" + Regex.Escape(".") + "pid" + Regex.Escape(".") + "[0-9]=0x" + pid, text.ToString());
@@ -94,20 +100,19 @@ namespace SelectDevice
                             //Add to DataGrid
                             this.dataGridView1.Rows.Add(port, b, name, pid, vid, sn);
                         }
-                        catch
-                        {
-                            Console.WriteLine("Error: Cannot open boards.txt");
-                        }
-
                     }
                 }
-            }
 
-            if (this.counter <= 1)
+                if (this.counter <= 1)
+                {
+                    #if (!DEBUG)
+                        this.Close();
+                    #endif
+                }
+            }
+            catch
             {
-                #if (!DEBUG)
-                    this.Close();
-                #endif
+                Console.WriteLine("Error: Cannot open boards.txt");
             }
 
         }
@@ -119,8 +124,10 @@ namespace SelectDevice
                 MessageBox.Show("Select a device in the above list");
             }else
             {
-                Console.WriteLine("set ARD_BOARD=\"--board arduino:avr:" + txtBoard.Text + "\"");
-                Console.WriteLine("set ARD_PORT=\"--port " + txtPort.Text + "\"");
+                //Console.WriteLine("sset ARD_BOARD=\"--board arduino:avr:" + txtBoard.Text + "\"");
+                //Console.WriteLine("sset ARD_PORT=\"--port " + txtPort.Text + "\"");
+
+                Console.WriteLine(txtBoard.Text + "," + txtPort.Text);
                 this.Close();
             }
             
