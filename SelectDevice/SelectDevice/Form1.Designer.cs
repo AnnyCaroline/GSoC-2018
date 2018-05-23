1a43af81d92a3fe59fa8a4c513c1280d3be07d59
Anny Caroline <annycarolinegnr@gmail.com>
2018-05-23 01:43:04 -0300

    Select device GUI app

---
+++ SelectDevice/SelectDevice/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace SelectDevice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Port = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.board = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.VID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtBoard = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Port,
+            this.board,
+            this.Name1,
+            this.PID,
+            this.VID,
+            this.SN});
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 29);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(694, 169);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // Port
+            // 
+            this.Port.HeaderText = "Port";
+            this.Port.Name = "Port";
+            this.Port.ReadOnly = true;
+            // 
+            // board
+            // 
+            this.board.HeaderText = "board";
+            this.board.Name = "board";
+            this.board.ReadOnly = true;
+            this.board.Visible = false;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Name";
+            this.Name1.Name = "Name1";
+            this.Name1.ReadOnly = true;
+            this.Name1.Width = 200;
+            // 
+            // PID
+            // 
+            this.PID.HeaderText = "PID";
+            this.PID.Name = "PID";
+            this.PID.ReadOnly = true;
+            // 
+            // VID
+            // 
+            this.VID.HeaderText = "VID";
+            this.VID.Name = "VID";
+            this.VID.ReadOnly = true;
+            // 
+            // SN
+            // 
+            this.SN.HeaderText = "SN";
+            this.SN.Name = "SN";
+            this.SN.ReadOnly = true;
+            this.SN.Width = 150;
+            // 
+            // txtPort
+            // 
+            this.txtPort.Enabled = false;
+            this.txtPort.Location = new System.Drawing.Point(25, 229);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 20);
+            this.txtPort.TabIndex = 3;
+            // 
+            // txtBoard
+            // 
+            this.txtBoard.Enabled = false;
+            this.txtBoard.Location = new System.Drawing.Point(151, 229);
+            this.txtBoard.Name = "txtBoard";
+            this.txtBoard.Size = new System.Drawing.Size(100, 20);
+            this.txtBoard.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Selected port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(148, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Selected board";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 210);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 42);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Select the device";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(742, 271);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoard);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.dataGridView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Céu-maker: select device";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.TextBox txtBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Port;
+        private System.Windows.Forms.DataGridViewTextBoxColumn board;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn VID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SN;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SelectDevice/SelectDevice/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Management;
+
+namespace SelectDevice
+{
+    public partial class Form1 : Form
+    {
+
+        private int selectedrowindex = -1;
+        private int counter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Helper function to handle regex search
+        private static string regex(string pattern, string text)
+        {
+            Regex re = new Regex(pattern);
+            Match m = re.Match(text);
+            if (m.Success)
+            {
+                return m.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string port = "", vid = "", pid = "", sn = "";
+
+            // Use WMI to get info
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher("root\\CIMV2",
+                "SELECT * FROM Win32_PnPEntity WHERE ClassGuid=\"{4d36e978-e325-11ce-bfc1-08002be10318}\"");
+
+            // Search all serial ports
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                // Parse the data
+                if (null != queryObj["Name"])
+                {
+                    //https://stackoverflow.com/questions/24135006/regex-that-match-any-character-inside-a-parenthesis
+                    port = regex("(?<=" + Regex.Escape("(") + ")[^)]*(?=" + Regex.Escape(")") + ")", queryObj["Name"].ToString());
+                }
+
+                if (null != queryObj["PNPDeviceID"])
+                {
+                    //https://stackoverflow.com/questions/3926451/how-to-match-but-not-capture-part-of-a-regex?rq=1
+                    vid = regex("(?<=VID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                    pid = regex("(?<=PID_)([0-9a-fA-F]+)", queryObj["PNPDeviceID"].ToString());
+                    sn = regex("([0-9a-fA-F]{5,})", queryObj["PNPDeviceID"].ToString());
+
+                    if (!string.IsNullOrEmpty(port) && !string.IsNullOrEmpty(vid) && !string.IsNullOrEmpty(pid) && !string.IsNullOrEmpty(sn))
+                    {
+                        counter++;
+
+                        try
+                        {
+                            #if DEBUG
+                                string text = System.IO.File.ReadAllText(@"C:\Users\AnnyC\Desktop\ceu-maker\ceu-maker\arduino-1.8.3\hardware\arduino\avr\boards.txt");
+                            #else
+                                string text = System.IO.File.ReadAllText(@"..\arduino-1.8.3\hardware\arduino\avr\boards.txt");
+                            #endif
+                        
+                            //Pega as duas linhas correspondentes ao VID e PID
+                            string a = "";
+                            a = regex("[a-zA-Z]+" + Regex.Escape(".") + "vid" + Regex.Escape(".") + "[0-9]=0x" + vid + "(\r)(\n)[a-zA-Z]+" + Regex.Escape(".") + "pid" + Regex.Escape(".") + "[0-9]=0x" + pid, text.ToString());
+
+                            //Pega a primeira palabra de [a]
+                            string b = "";
+                            b = regex("[a-zA-Z]+(?=" + Regex.Escape(".") + "vid)", a.ToString());
+
+                            //Pesquisa b.build.board
+                            string board = "";
+                            board = regex("(?<=" + b + Regex.Escape(".") + "build" + Regex.Escape(".") + "board=)(\\w)+", text.ToString());
+
+                            //Pesquisa b.name
+                            string name = "";
+                            name = regex("(?<=" + b + Regex.Escape(".") + "name=).+", text.ToString());
+
+                            //Add to DataGrid
+                            this.dataGridView1.Rows.Add(port, b, name, pid, vid, sn);
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Error: Cannot open boards.txt");
+                        }
+
+                    }
+                }
+            }
+
+            if (this.counter <= 1)
+            {
+                #if (!DEBUG)
+                    this.Close();
+                #endif
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtPort.Text=="" || txtBoard.Text=="" || selectedrowindex == -1)
+            {
+                MessageBox.Show("Select a device in the above list");
+            }else
+            {
+                Console.WriteLine("set ARD_BOARD=\"--board arduino:avr:" + txtBoard.Text + "\"");
+                Console.WriteLine("set ARD_PORT=\"--port " + txtPort.Text + "\"");
+                this.Close();
+            }
+            
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            //https://stackoverflow.com/questions/7657137/datagridview-full-row-selection-but-get-single-cell-value
+            this.selectedrowindex = dataGridView1.SelectedCells[0].RowIndex;
+
+            DataGridViewRow selectedRow = dataGridView1.Rows[selectedrowindex];
+            txtPort.Text = Convert.ToString(selectedRow.Cells["port"].Value);
+            txtBoard.Text = Convert.ToString(selectedRow.Cells["board"].Value);
+        }
+    }
+}
